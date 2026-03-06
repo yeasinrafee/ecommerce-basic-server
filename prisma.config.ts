8a52f4f1b0635491118e49decd27afe3fcf0caa7
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2026-03-06 17:58:24 +0600

    refactor: update Prisma configuration and add schema files

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,10 @@
+
 import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+ 
+  schema: 'prisma/schema', 
   migrations: {
     path: 'prisma/migrations'
   },
